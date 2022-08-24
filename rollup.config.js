54a8818b2405039f9f3f09ec1e34cddd7d8793f2
lianwen.wu <1580940252@qq.com>
2022-08-24 17:37:15 +0800

    🐞 fix: remove autoload

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 const core = require("./packages/core/rollup.config");
-const autoload = require("./packages/autoload/rollup.config");
 
 module.exports = function rollup() {
-  return [core(), ...autoload()];
+  return [core()];
 };
